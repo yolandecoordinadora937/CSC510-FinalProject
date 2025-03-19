0c4e79ffb77d1847232d8606e43eefdbf3ce6b6a
yolandecoordinadora937 <yolandecoordinadora937@gmail.com>
2025-03-19 21:01:14 +0000

    Add 13.ts

---
+++ src/13.ts
@@ -0,0 +1,16 @@
+import { useState } from "react";
+
+function Example() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={handleClick}>Click me</button>
+    </div>
+  );
+}
